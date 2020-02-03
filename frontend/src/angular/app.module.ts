1f3dca6d0242659f24a1841b063e286c595da031
Byron Farrell <byronfarrell22@gmail.com>
2020-02-03 16:35:34 +0000

    map and navbar components add, more work still needs to be done

--- frontend/src/angular/app.module.ts
+++ frontend/src/angular/app.module.ts
@@ -5,18 +5,22 @@ import { MapComponent } from './components/map/map.component';
 import { GraphComponent } from './components/graph/graph.component';
 import { MapRootComponent } from './components/map-root/map-root.component';
 import { GraphRootComponent } from './components/graph-root/graph-root.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { RootComponent } from './components/root/root.component';
 
 @NgModule({
   declarations: [
     MapComponent,
     GraphComponent,
     MapRootComponent,
-    GraphRootComponent
+    GraphRootComponent,
+    NavbarComponent,
+    RootComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [MapComponent]
+  bootstrap: [RootComponent]
 })
 export class AppModule { }
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,12 +1,9 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import 'bootstrap';
 
 import { AppModule } from './angular/app.module';
-import { environment } from './environments/environment';
 
-if (environment.production) {
-  enableProdMode();
-}
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
