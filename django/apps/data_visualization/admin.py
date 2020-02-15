b8eac6325430cc7a5f269312e0ce538b8d246d27
Byron Farrell <byronfarrell22@gmail.com>
2020-02-15 18:02:23 +0000

    made admin panel more user friendly by displying data in a list form and the logs can now be filtered

--- django/apps/data_visualization/admin.py
+++ django/apps/data_visualization/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 from . import models
 
 
+class AuthorAdmin(admin.ModelAdmin):
+    date_hierarchy = 'timestamp'
+    list_display = ('type', 'message', 'row', 'fileName', 'timestamp')
+    list_filter  = ('type', 'fileName', 'timestamp')
+
+
 # Register your models here.
-admin.site.register(models.Log)
+admin.site.register(models.Log, AuthorAdmin)
