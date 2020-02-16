15f6c98c02c316e0f03322c2d3171163c57368c1
Byron Farrell <byronfarrell22@gmail.com>
2020-02-16 20:54:33 +0000

    date selector setup and create component for filter options headers

--- frontend/src/TS/angular/app.module.ts
+++ frontend/src/TS/angular/app.module.ts
@@ -1,13 +1,23 @@
+// --------------- ANGULAR ---------------
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
+// -------------- COMPONENTS --------------
 import { MapComponent } from './components/map/map.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { RootComponent } from './components/root/root.component';
 import { CheckboxSelectorComponent } from './components/checkbox-selector/checkbox-selector.component';
+import { DateSelectorComponent } from './components/date-selector/date-selector.component';
 import { GenericFilterOptionsComponent } from './components/generic-filter-options/generic-filter-options.component';
 import { MapSelectorComponent } from './components/map-selector/map-selector.component';
+import { FilterOptionHeaderComponent } from './components/filter-option-header/filter-option-header.component';
+
+// ----------- ANGULAR MATERIAL -----------
+import { MatNativeDateModule } from '@angular/material';
+import { MatDatepickerModule} from '@angular/material/datepicker';
+import { MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 
 @NgModule({
@@ -18,10 +28,16 @@ import { MapSelectorComponent } from './components/map-selector/map-selector.com
     CheckboxSelectorComponent,
     GenericFilterOptionsComponent,
     MapSelectorComponent,
+    DateSelectorComponent,
+    FilterOptionHeaderComponent,
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatInputModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [RootComponent]
---
+++ frontend/src/TS/angular/components/filter-option-header/filter-option-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-filter-option-header',
+  templateUrl: './filter-option-header.component.html',
+  styleUrls: ['./filter-option-header.component.scss']
+})
+export class FilterOptionHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
