9fb140cf123457b31a8f94b604ceffd1db8b49c8
Byron Farrell <byronfarrell22@gmail.com>
2020-02-17 19:17:17 +0000

    side panel now now be hidden

--- frontend/src/TS/angular/components/root/root.component.ts
+++ frontend/src/TS/angular/components/root/root.component.ts
@@ -7,7 +7,10 @@ import { Component, OnInit, AfterViewInit } from '@angular/core';
 })
 export class RootComponent implements OnInit, AfterViewInit {
 
+  hideSidePanel: boolean;
+
   constructor() {
+    this.hideSidePanel = true;
   }
 
   ngOnInit() {
@@ -30,9 +33,32 @@ export class RootComponent implements OnInit, AfterViewInit {
 
   onResize(event) : void {
     this.calculateContentHeight();
+
+
+  }
+
+  toggleSidePanel(event) {
+    let sidePanel = document.getElementById('sidePanel');
+    let mainContent = document.getElementById('mainContent');
+    let expandArrow = document.getElementById('expand');
+
+    if (this.hideSidePanel) {
+      sidePanel.className = 'd-none';
+      mainContent.className = 'col-sm-12 col-md-12 col-lg-12';
+      expandArrow.className = 'm-2 position-absolute';
+      this.hideSidePanel = !this.hideSidePanel;
+    }
+    else {
+      sidePanel.className = 'col-sm-12 col-md-5 col-lg-3 h-100 overflow-auto';
+      mainContent.className = 'col-sm-12 col-md-7 col-lg-9';
+      expandArrow.className = 'd-none';
+      this.hideSidePanel = !this.hideSidePanel;
+    }
+
+    window.dispatchEvent(new Event('resize'));
   }
 
   onSelect(): void {
-    
+
   }
 }
