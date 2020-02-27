912706df8fb6d62e6ccea935f503cdc6c0a23cc6
Byron Farrell <byronfarrell22@gmail.com>
2020-02-27 20:03:53 +0000

    update

--- frontend/src/TS/angular/components/data-upload/column-mapping/column-mapping.component.ts
+++ frontend/src/TS/angular/components/data-upload/column-mapping/column-mapping.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+
 // --------------- SERVICES ---------------
 import { FileUploadService } from '../../../services/file-upload.service';
 @Component({
@@ -11,17 +12,45 @@ export class ColumnMappingComponent implements OnInit {
   public fileData: any;
   public columns: Array<string>;
 
+  private criminalColumns: Array<string>;
+  private censusBlockColumns: Array<string>;
+  private censusInformationColumns: Array<string>;
+
   constructor(private fileUploadService: FileUploadService) { }
 
   ngOnInit() {
-    this.columns = [
+    this.criminalColumns = [
       'Crime Type',
       'Crime Description',
       'Arrest',
       'Longitude',
       'Latitude',
       'Date'
-    ]
+    ];
+
+    this.censusBlockColumns = [
+      'Block Id',
+      'Geo'
+    ];
+
+    this.censusInformationColumns = [
+      'Block Id',
+      'Deprevation Index'
+    ];
+
+    // if (this.dataType === 'Criminal Data')
+    // {
+    //   this.columns = this.criminalColumns;
+    // }
+    // else if (this.dataType === 'Census Blocks') {
+    //   this.columns = this.censusBlockColumns;
+    // }
+    // else if (this.dataType === 'Census Information') {
+    //   this.columns = this.censusInformationColumns;
+    // }
+
+    this.columns = this.criminalColumns;
+    
     this.fileData = this.fileUploadService.getData();
 
   }
