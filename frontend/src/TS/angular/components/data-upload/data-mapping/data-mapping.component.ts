8eaaa531b5715b4537f2c8e533e45a256b7132a1
Byron Farrell <byronfarrell22@gmail.com>
2020-03-04 15:06:59 +0000

    update data mapping component

--- frontend/src/TS/angular/components/data-upload/data-mapping/data-mapping.component.ts
+++ frontend/src/TS/angular/components/data-upload/data-mapping/data-mapping.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 // --------------- SERVICES ---------------
 import { FileUploadService } from '../../../services/file-upload.service';
+import { CrimeService } from '../../../services/crime.service';
 
 @Component({
   selector: 'app-data-mapping',
@@ -10,12 +11,43 @@ import { FileUploadService } from '../../../services/file-upload.service';
 })
 export class DataMappingComponent implements OnInit {
 
-  constructor(private fileUploadService: FileUploadService) { }
+  public columnMappings: any;
+  public cities: Array<string>;
+  public dropdownStyles: string;
+  public cityMappingName: string;
+  public userCrimeTypes: Array<string>;
+  public applicationCrimeTypes: Array<string>;
+
+  constructor(
+    private fileUploadService: FileUploadService,
+    private crimeService: CrimeService
+  ) { }
 
   ngOnInit() {
-    let temp = this.fileUploadService.getColumnMappings();
-    console.log(temp);
-
+    this.setup();
   }
 
+  private setup(): void {
+    this.columnMappings = this.fileUploadService.getColumnMappings();
+    this.dropdownStyles = 'btn btn-secondary w-100';
+    this.cityMappingName = 'City';
+    this.applicationCrimeTypes = Array();
+    this.cities = Array();
+    
+    this.crimeService.loadCities()
+      .then(json => {
+        json.forEach(type => {
+          this.cities.push(type);
+        })
+      })
+      .catch(error => console.error(error));
+
+    this.crimeService.loadCrimeTypes()
+      .then(json => {
+        json.forEach(type => {
+          this.applicationCrimeTypes.push(type);
+        })
+      })
+      .catch(error => console.error(error));
+  }
 }
